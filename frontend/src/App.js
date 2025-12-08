dd77e0176d7f5181e3ae3db747194feec36d1872
SarahDebruyne <sarahdebruyne5@gmail.com>
2025-12-08 14:19:52 +0100

    Add React Router and basic routing for pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import ListArtisans from './pages/ListArtisans';
+import ArtisanPage from './pages/ArtisanPage';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        {/* Page d'accueil */}
+        <Route path="/" element={<Home />} />
+
+        {/* Liste des artisans */}
+        <Route path="/artisans" element={<ListArtisans />} />
+
+        {/* Fiche artisan avec un paramètre :id */}
+        <Route path="/artisan/:id" element={<ArtisanPage />} />
+
+        {/* Route de secours (404) */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
