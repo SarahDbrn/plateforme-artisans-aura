7a2265040df8ca0d0680b3e674fb9c84f26cd428
SarahDebruyne <sarahdebruyne5@gmail.com>
2025-12-09 14:56:40 +0100

    modification cards artisans du mois

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './styles/main.scss';
 import App from './App';
 import 'bootstrap-icons/font/bootstrap-icons.css';
