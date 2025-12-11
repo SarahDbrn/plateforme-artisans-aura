2bd35059c54aac5e574bea8bf01a09a49d7806de
SarahDebruyne <sarahdebruyne5@gmail.com>
2025-12-11 07:15:42 +0100

    update database

--- backend/src/server.js
+++ backend/src/server.js
@@ -41,3 +41,8 @@ sequelize
   .catch((err) => {
     console.error('Erreur de connexion à la base :', err);
   });
+
+  console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
